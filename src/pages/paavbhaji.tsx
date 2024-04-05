48120c78997be6b435fb653d7c4a00ff5ec496c9
webbuildershopiana <webbuilder.shopiana@gmail.com>
2024-04-05 07:53:38 +0000

    first commit

--- src/pages/paavbhaji.tsx
+++ src/pages/paavbhaji.tsx
@@ -84,37 +84,37 @@ const paavbhajiPage = ({
     <div id="ii6c8l">
      Method
     </div>
-    <p id="itt6nq">
+    <p className="mt-4" id="itt6nq">
      Step 1
     </p>
     <p id="ihp77f">
      Place a pizza stone in the lower third of the oven and preheat to 500°. Allow the stone to heat for at least half an hour before baking pizzas.
     </p>
-    <p id="irnnem">
+    <p className="mt-4" id="irnnem">
      Step 2
     </p>
     <p id="iohern">
      Allow the pizza dough to come to room temperature. On a lightly-floured work surface, form the dough into two large balls, collecting the sides and tucking them under to create a smooth outer surface. Cover the dough with a damp towel and allow to rest for at least 20 minutes.
     </p>
-    <p id="ik0xyn">
+    <p className="mt-4" id="ik0xyn">
      Step 3
     </p>
     <p id="ia9qbc">
      Meanwhile, blanch the potato slices in boiling water until just cooked through, anywhere between 1 – 2 minutes. Drain and allow to cool slightly before drying the slices with paper towels and coating them with a touch of olive oil, a pinch of Italian seasoning and kosher salt.
     </p>
-    <p id="i5frh8">
+    <p className="mt-4" id="i5frh8">
      Step 4
     </p>
     <p id="i50w3v">
      Generously dust a wooden peel or the back of a sheet pan with flour or cornmeal. With lightly-floured hands, press the dough with your fingertips to form a flattened disk. Lift the dough up and use the back of your knuckles to stretch and thin-out the dough into a circle with a diameter of 12-inches. Be careful to preserve the edge of the dough if you want a light, chewy crust. Carefully position the stretched dough onto the prepared peel or baking sheet.
     </p>
-    <p id="i4y83l">
+    <p className="mt-4" id="i4y83l">
      Step 5
     </p>
     <p id="iv0m6a">
      Using a large spoon, spread an even layer of the pesto onto the dough and carefully spread it to within 1/2-inch of the edge, being careful to leave a sauce free edge. Top with an even layer of the grated mozzarella/provolone cheese followed by slices of parboiled potatoes and roasted garlic cloves. Top with a light sprinkling of a bit more mozzarella, season with salt to taste and a pinch of red pepper flakes. Lightly brush the exposed crust with a bit of olive oil (or leftover garlic) oil to promote browning.
     </p>
-    <p id="i6upgf">
+    <p className="mt-4" id="i6upgf">
      Step 6
     </p>
     <p id="ilzc12">
