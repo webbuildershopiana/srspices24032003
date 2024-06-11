66a100dbb0fe1225459b3a18fc80f85b8d762d77
webbuildershopiana <webbuilder.shopiana@gmail.com>
2024-06-11 07:19:16 +0000

    first commit

--- public/assets/script.js
+++ public/assets/script.js
@@ -9,7 +9,7 @@ const t=this.querySelectorAll(".hamburger1"),e=this.querySelectorAll(".navbar-co
             (function(){
 var myCarousel = document.querySelector('#carouselExample');
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 1000,
+  interval: 1500,
   ride: 'carousel'
 }
                                      );
